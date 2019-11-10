d4d3ce393dedcc101574a14172485b46bc91276e
maxwen <max.weninger@gmail.com>
2019-11-10 09:11:57 +0100

    soong: add support for building product overlay files
    add product_overlay_specific: true to bp file to install files
    into /prodict/vendor_overlay/<version>
    
    Change-Id: I38c25a99c6a9aeee7686881427dff88cba8feb1c
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- android/paths.go
+++ android/paths.go
@@ -1781,6 +1781,8 @@ func modulePartition(ctx ModuleInstallPathContext, os OsType) string {
 			partition = ctx.DeviceConfig().SystemExtPath()
 		} else if ctx.InstallInRoot() {
 			partition = "root"
+		} else if ctx.ProductOverlaySpecific() {
+			partition = ctx.DeviceConfig().ProductOverlayPath()
 		} else {
 			partition = "system"
 		}
